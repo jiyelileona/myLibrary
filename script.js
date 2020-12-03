7c75fb0c40ac6d444f9beaf8be5c8392476f5b9d
Jiye Li <15832778446@163.com>
2020-12-02 18:19:48 -0600

    adds form and nav bar button style, adds event listener to nav bar button

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+const addBookButton = document.querySelector('.button')
+
+addBookButton.addEventListener('click', function() {
+  form.classList.toggle('open')
+})
