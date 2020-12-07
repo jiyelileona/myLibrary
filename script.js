9a71e24eacfff3841923e39fc2ddd738acef5e54
Jiye Li <15832778446@163.com>
2020-12-06 22:17:36 -0600

    add rotate animation to add book button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
-const addBookButton = document.querySelector('.button')
+const addBookButton = document.querySelector('.button');
 
-addBookButton.addEventListener('click', function() {
-  form.classList.toggle('open')
-})
+addBookButton.addEventListener('click', function () {
+  form.classList.toggle('open');
+  addBookButton.classList.toggle('open');
+});
