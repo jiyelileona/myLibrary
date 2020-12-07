f94d1737fea1be45adb996e3d2bf6bffafabafaf
Jiye Li <15832778446@163.com>
2020-12-07 10:03:01 -0600

    add book class and library

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const form = document.querySelector('form');
 const addBookButton = document.querySelector('.button');
+const submitButton = document.querySelector('#submitButton');
+const bookName = document.querySelector('#bookName');
+const author = document.querySelector('#author');
+const pagesNum = document.querySelector('#pagesNumber');
+
+let library = [];
+
+class Book {
+  constructor(bookName, author, pagesNum, isReaded) {
+    this.bookName = bookName;
+    this.author = author;
+    this.pagesNum = pagesNum;
+    this.isReaded = isReaded;
+  }
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  document.querySelector('input[name = "readed"]:checked').value === 'true' ? true : false;
+  let isReaded = document.querySelector('input[name = "readed"]:checked').value;
+  const newBook = new Book(bookName.value, author.value, pagesNum.value, isReaded)
+  library.push(newBook);
+});
+
 
 addBookButton.addEventListener('click', function () {
   form.classList.toggle('open');
